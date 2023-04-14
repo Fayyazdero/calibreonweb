ed509da51dfd93f101264f493c404e37a746c05c
Qasim Fida <qasimfida0054@gmail.com>
2023-04-14 16:58:37 +0500

    Removed babel and styled-components config

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,11 @@ import { getTheme, theme } from "@/theme";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  // Add your global styles here
+  body {
+    font-family: "Hind Madurai", sans-serif;
+    font-family: "Montserrat", sans-serif;
+    font-family: "Poppins", sans-serif;
+  }
 `;
 
 function MyApp({ Component, pageProps }) {
