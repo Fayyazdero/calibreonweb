e856cd5a4de80c022001ed974b25490c63ea7829
Zahid590 <officialzahid590@gmail.com>
2023-04-24 19:40:03 +0500

    create a responsive header on mobile version

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,8 +4,20 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "/public/logo.svg";
 import { useState } from "react";
-import { LinksWrapper, NavButtonsWrapper, StyledLink } from "./styles";
+import {
+  LinksWrapper,
+  NavButtonsWrapper,
+  NavbarIconWrapper,
+  SocialIcons,
+  SocialTitle,
+  SocialWrapper,
+  StyledLink,
+  StyledNavbarCollapse,
+  Overlay,
+  ToggleButton,
+} from "./styles";
 import Button from "../Button";
+import { CrossIcon, Facebook, Instagram, LinkedIn, Twitter } from "../Svgs";
 
 function Header() {
   let navLinks = [
@@ -32,26 +44,39 @@ function Header() {
   ];
 
   const [active, setActive] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = (index) => {
     setActive(index);
   };
+
+  const handleMenuToggle = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
-    <Navbar bg="white" expand="lg">
+    <Navbar bg="white" expand="lg" expanded={isOpen}>
       <Container>
         <Navbar.Brand href="#home">
           <Image src={logo} alt="logo" height="100%" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse
+        <ToggleButton
+          aria-controls="basic-navbar-nav"
+          onClick={handleMenuToggle}
+        />
+        <StyledNavbarCollapse
           className="justify-content-center"
           id="basic-navbar-nav"
         >
           <Nav>
+            <Overlay />
             {navLinks.map((item, key) => (
               <LinksWrapper key={key}>
                 <StyledLink
-                  onClick={() => handleClick(key)}
+                  onClick={() => {
+                    handleClick(key);
+                    setIsOpen(false);
+                  }}
                   className={`mx-3 text-decoration-none text-dark styled-link ${
                     active === key ? "active" : ""
                   }`}
@@ -62,15 +87,27 @@ function Header() {
               </LinksWrapper>
             ))}
           </Nav>
-        </Navbar.Collapse>
-        <NavButtonsWrapper>
-          <Button className="mx-2" variant="contained">
-            Login
-          </Button>
-          <Button className="mx-2" variant="outlined">
-            Sign UP
-          </Button>
-        </NavButtonsWrapper>
+          <NavButtonsWrapper>
+            <Button className="mx-2" variant="contained">
+              Login
+            </Button>
+            <Button className="mx-2" variant="outlined">
+              Sign UP
+            </Button>
+          </NavButtonsWrapper>
+          <SocialWrapper>
+            <SocialTitle>Join the community</SocialTitle>
+            <SocialIcons>
+              <Instagram height={31} color="#4D4D4D" />
+              <Twitter height={25} color="#4D4D4D" />
+              <Facebook height={25} color="#4D4D4D" />
+              <LinkedIn height={23} color="#4D4D4D" />
+            </SocialIcons>
+          </SocialWrapper>
+          <NavbarIconWrapper onClick={handleMenuToggle}>
+            <CrossIcon height={13} width={13} />
+          </NavbarIconWrapper>
+        </StyledNavbarCollapse>
       </Container>
     </Navbar>
   );
