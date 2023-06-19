7299dbbe3abf765ec21f0ecfc675ef202a61fbe5
fhyem <fhyembercha@gmail.com>
2023-06-19 15:35:49 +0500

    small change

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -184,6 +184,7 @@ function Header() {
                         {item.title}
                       </StyledLink>
                     </LinksWrapperMobile>
+                    
                     <NavButtonsWrapperForMobile>
                       <Button
                         className="mx-2"
